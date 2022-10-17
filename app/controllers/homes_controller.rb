04550a27bf2eba821bcb5f6fa3728b4ad192a6d7
Awais Sajid <awaisror217@gmail.com>
2022-10-17 22:04:05 +0500

    Added Emplyment Messages to controller & Refector Code

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,9 @@ class HomesController < ApplicationController
 
   MESSAGES = {
     created: 'Personal Data has been created successfully.',
-    not_created: 'Personal Data has not been created successfully.'
+    emp_created: 'Employment has been created successfully.',
+    not_created: 'Personal Data has not been created successfully.',
+    emp_not_created: 'Employment has not been created successfully.',
   }.freeze
 
   def index
@@ -10,21 +12,33 @@ class HomesController < ApplicationController
 
   def create_personal_data
     personal_data = PersonalData.new(personal_data_params)
-    personal_data.save
+    return flash[:error] = MESSAGES[:not_created] unless personal_data.save
+
+    flash[:success] = MESSAGES[:created]
+    redirect_root
   end
 
   def create_employment
     employment = Employment.new(employment_params)
-    employment.save
+    return flash[:error] = MESSAGES[:emp_not_created] unless employment.save
+
+    flash[:success] = MESSAGES[:emp_created]
+    redirect_root
   end
 
   private
 
   def personal_data_params
-    params.require(:personal_data).permit(:nick_name, :phone_number, :email_address, :last_name, :first_name)
+    params.require(:personal_data).permit(:nick_name, :phone_number,
+                                          :email_address, :last_name,
+                                          :first_name)
   end
 
   def employment_params
     params.require(:employment).permit(:employer, :date_started, :date_employment_ended)
+  end
+
+  def redirect_root
+    redirect_to root_url
   end
 end
